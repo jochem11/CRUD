815e1a7db14e58d2dc012673630df422ca980263
jochem11 <0195765@student.roc-nijmegen.nl>
2022-05-10 12:57:28 +0200

    idk

--- index.php
+++ index.php
@@ -6,8 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRUD</title>
     <link rel="stylesheet" href="css/styles.css">
+    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
+    <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
 </head>
 <body>
+    <div class="navbar">
+        <div class="linkjesRechts">
+            
+        </div>
+    <!-- <i class="fa-solid fa-person-circle-plus"></i>   -->
+    </div>
     <script src="js/main.js"></script>
+    <script src="https://kit.fontawesome.com/33689bd479.js" crossorigin="anonymous"></script>
 </body>
 </html>
