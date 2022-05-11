e0393121a249da87817659c8f0406f2bd32e05e6
jochem11 <0195765@student.roc-nijmegen.nl>
2022-05-11 13:58:51 +0200

    container invul

--- index.php
+++ index.php
@@ -1,36 +1,76 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>CRUD Airlines</title>
-    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
-    <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
-    <link rel="stylesheet" href="css/styles.css">
-</head>
-<body>
+    <link rel="stylesheet" href="css/styles.css" />
+  </head>
+  <body>
     <div class="navbar">
-        <div class="logoContainer">
-            <img src="pictures/nieuwe_logo.png" alt="logo">
-            <h1>CRUDairlines</h1>
+      <div class="logoContainer">
+        <img src="pictures/nieuwe_logo.png" alt="logo" />
+        <h1>CRUDairlines</h1>
+      </div>
+      <div class="linkjesRechts">
+        <div class="contact">
+          <i class="fa-solid fa-id-card-clip"></i>
+          <p>contact</p>
         </div>
-        <div class="linkjesRechts">
-            <div class="contact">
-                <i class="fa-solid fa-id-card-clip"></i>
-                <p>contact</p>
-            </div>
-            <div class="login">
-                <i class="fa-solid fa-user-tie"></i>
-                <p>login</p>
-            </div>
-            <div class="login">
-                <i class="fa-solid fa-person-circle-plus"></i>  
-                <p>sign up</p>
-            </div>
+        <div class="login">
+          <i class="fa-solid fa-user-tie"></i>
+          <p>login</p>
         </div>
+        <div class="login">
+          <i class="fa-solid fa-person-circle-plus"></i>
+          <p>sign up</p>
+        </div>
+      </div>
+    </div>
+    <div class="slideshow-container">
+      <div class="mySlides fade">
+        <img
+          src="pictures/slideshow/slider_familie.jpg"
+          alt="slideshow familie"
+        />
+      </div>
+
+      <div class="mySlides fade">
+        <img
+          src="pictures/slideshow/slider_new york .jpg"
+          alt="slideshow new york"
+        />
+      </div>
+
+      <div class="mySlides fade">
+        <img
+          src="pictures/slideshow/slider_terminal.jpg"
+          alt="slideshow terminal"
+        />
+      </div>
+
+      <div class="mySlides fade">
+        <img
+          src="pictures/slideshow/slider_vliegtuig.jpg"
+          alt="slideshow vliegtuig"
+        />
+      </div>
+      <div class="boekcontainer">
+        <form action="" method="post">
+          <div class="rij1">
+            <div class="van-waar">
+              <input type="text" name="" id="" />
+            </div>
+            <div class="van-waar"></div>
+          </div>
+        </form>
+      </div>
     </div>
     <script src="js/main.js"></script>
-    <script src="https://kit.fontawesome.com/33689bd479.js" crossorigin="anonymous"></script>
-</body>
+    <script
+      src="https://kit.fontawesome.com/33689bd479.js"
+      crossorigin="anonymous"
+    ></script>
+  </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-var slideIndex = 0;
+let slideIndex = 0;
 showSlides();
 
 function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
 
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
@@ -14,5 +14,5 @@ function showSlides() {
   }
 
   slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 3000);
+  setTimeout(showSlides, 5000);
 }
