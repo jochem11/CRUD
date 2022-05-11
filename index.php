130ccbd06cebfb2e419d7354ceacd14189f52be5
jochem11 <0195765@student.roc-nijmegen.nl>
2022-05-11 09:42:21 +0200

    navbar

--- index.php
+++ index.php
@@ -4,17 +4,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CRUD</title>
-    <link rel="stylesheet" href="css/styles.css">
+    <title>CRUD Airlines</title>
     <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
     <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
     <div class="navbar">
-        <div class="linkjesRechts">
-            
+        <div class="logoContainer">
+            <img src="pictures/nieuwe_logo.png" alt="logo">
+            <h1>CRUDairlines</h1>
         </div>
-    <!-- <i class="fa-solid fa-person-circle-plus"></i>   -->
+        <div class="linkjesRechts">
+            <div class="contact">
+                <i class="fa-solid fa-id-card-clip"></i>
+                <p>contact</p>
+            </div>
+            <div class="login">
+                <i class="fa-solid fa-user-tie"></i>
+                <p>login</p>
+            </div>
+            <div class="login">
+                <i class="fa-solid fa-person-circle-plus"></i>  
+                <p>sign up</p>
+            </div>
+        </div>
     </div>
     <script src="js/main.js"></script>
     <script src="https://kit.fontawesome.com/33689bd479.js" crossorigin="anonymous"></script>
